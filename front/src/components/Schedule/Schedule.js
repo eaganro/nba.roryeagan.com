07f01266aa0923300e6b60f1a1135a5bff05b104
eaganro <rory.eagan@gmail.com>
2023-10-26 15:41:38 -0400

    Layout stying consistent with other sections. Date picker min width. Text no wrap. Scroll on game list overflow.

--- front/src/components/Schedule/Schedule.js
+++ front/src/components/Schedule/Schedule.js
@@ -1,22 +1,21 @@
 import './Schedule.scss';
 
-import Button from '@mui/material/Button';
-
 export default function Schedule({ games, date, changeDate, changeGame }) {
 
   const gamesList = games.map(g => {
     return (
       <div key={g.gameId} onClick={() => changeGame(g.gameId)}>
-        <div>{g.away} - {g.home}</div>
+        {g.away} - {g.home}
       </div>
     )
   });
 
   return (
     <div className='schedule'>
-      <input type="date" value={date} onChange={changeDate}></input>
-      {gamesList}
-      <Button variant="text">Text</Button>
+      <input className='dateInput' type="date" value={date} onChange={changeDate}></input>
+      <div className="games">
+        {gamesList}
+      </div>
     </div>
   );
 }
