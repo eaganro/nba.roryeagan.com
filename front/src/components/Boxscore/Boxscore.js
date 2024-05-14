9f56c135a9181897758241210ed5d4c11b11748f
eaganro <rory.eagan@gmail.com>
2024-05-14 14:27:12 -0400

    Shrink box score name column on screen width change.

--- front/src/components/Boxscore/Boxscore.js
+++ front/src/components/Boxscore/Boxscore.js
@@ -1,12 +1,21 @@
 import './Boxscore.scss';
 import processTeamStats from './processTeamStats';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export default function Boxscore({ box }) {
   const [showMore, setShowMore] = useState(false);
   const [scrollPos, setScrollPos] = useState(100);
-  console.log('state', scrollPos)
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    console.log(width);
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [width]);
 
   const awayBox = processTeamStats(box?.awayTeam, false, showMore, setShowMore, scrollPos, setScrollPos)
   const homeBox = processTeamStats(box?.homeTeam, true, showMore, setShowMore, scrollPos, setScrollPos)
