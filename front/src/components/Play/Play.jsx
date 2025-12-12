e5d2ef1c41b551faf5f064df3cc47cc804afea7c
eaganro <rory.eagan@gmail.com>
2025-12-11 19:47:40 -0500

    Update strokeDasharray for timeline lines to improve visual distinction between lines

--- front/src/components/Play/Play.jsx
+++ front/src/components/Play/Play.jsx
@@ -308,11 +308,11 @@ export default function Play({ awayTeamNames, homeTeamNames, awayPlayers, homePl
 
   for (let i = 0; i < numLines; i += 1) {
     let posy = 300 + ((i + 1) * lineJump) * - 300 / maxY
-    timeline.unshift(<line key={`sp${i}-${awayTeamName}-${homeTeamName}`} x1={leftMargin - 5} y1={posy} x2={leftMargin + width - 5} y2={posy} strokeDasharray={"5,5"} style={{ stroke: teamColors.away, strokeWidth: 0.5 }} />)
+    timeline.unshift(<line key={`sp${i}-${awayTeamName}-${homeTeamName}`} x1={leftMargin - 5} y1={posy} x2={leftMargin + width - 5} y2={posy} strokeDasharray={"5,20"} style={{ stroke: teamColors.away, strokeWidth: 0.5 }} />)
     timeline.unshift(<text key={`sp-label-${i}`} x={leftMargin + width + 10} y={posy + 4} textAnchor="end" style={{ ...quarterLabelStyle, fill: teamColors.away }}>{(i + 1) * lineJump}</text>)
     
     let negy = 300 + (-1 * (i + 1) * lineJump) * - 300 / maxY
-    timeline.unshift(<line key={`sn${i}-${awayTeamName}-${homeTeamName}`} x1={leftMargin - 5} y1={negy} x2={leftMargin + width - 5} y2={negy} strokeDasharray={"5,5"} style={{ stroke: teamColors.home, strokeWidth: 0.5 }} />)
+    timeline.unshift(<line key={`sn${i}-${awayTeamName}-${homeTeamName}`} x1={leftMargin - 5} y1={negy} x2={leftMargin + width - 5} y2={negy} strokeDasharray={"5,20"} style={{ stroke: teamColors.home, strokeWidth: 0.5 }} />)
     timeline.unshift(<text key={`sn-label-${i}`} x={leftMargin + width + 10} y={negy + 4} textAnchor="end" style={{ ...quarterLabelStyle, fill: teamColors.home }}>{(i + 1) * lineJump}</text>)
   }
 
