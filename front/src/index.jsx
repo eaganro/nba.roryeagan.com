cd2ae5c112834782b1788336af8fc7e4249a89e7
eaganro <rory.eagan@gmail.com>
2025-12-22 16:58:10 -0500

    Add google analytics.

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,7 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import { ThemeProvider } from './components/hooks/useTheme';
+import ReactGA from 'react-ga4';
 import './theme.scss';
+
+const gaId = import.meta.env.VITE_GA_ID;
+if (gaId) {
+  ReactGA.initialize(gaId);
+  ReactGA.send({
+    hitType: 'pageview',
+    page: `${window.location.pathname}${window.location.search}${window.location.hash}`,
+    title: document.title,
+  });
+}
 
 function RootComponent() {
   return (
